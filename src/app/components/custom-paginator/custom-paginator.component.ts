1c164b95bb6cd99f2d60cd620762b654c415ef54
Kholodulin Maxim <maxotron-0@list.ru>
2024-06-30 22:17:23 +0300

    Added Paginator dynamic current page number display

--- src/app/components/custom-paginator/custom-paginator.component.ts
+++ src/app/components/custom-paginator/custom-paginator.component.ts
@@ -8,13 +8,14 @@ import {
 import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-custom-paginator',
   standalone: true,
   templateUrl: './custom-paginator.component.html',
-  styleUrl: './custom-paginator.component.css',
-  imports: [DropdownModule, FormsModule, ButtonModule],
+  styleUrl: './custom-paginator.component.scss',
+  imports: [DropdownModule, FormsModule, ButtonModule, CommonModule],
 })
 export class CustomPaginatorComponent<T> implements OnChanges {
   @Input() data: T[] = [];
@@ -74,6 +75,29 @@ export class CustomPaginatorComponent<T> implements OnChanges {
   onRowsCountChanged() {
     this.currentPage = 0;
     this.totalPages = Math.ceil(this.data.length / this.selectedRowsCount);
+    this.emitDataForShow();
+  }
+
+  getPagesArray(): number[] {
+    const totalPagesArray = Array.from(
+      { length: this.totalPages },
+      (_, i) => i + 1
+    );
+    if (this.totalPages <= 5) {
+      return totalPagesArray;
+    }
+
+    if (this.currentPage < 3) {
+      return totalPagesArray.slice(0, 5);
+    } else if (this.currentPage > this.totalPages - 3) {
+      return totalPagesArray.slice(this.totalPages - 5, this.totalPages);
+    } else {
+      return totalPagesArray.slice(this.currentPage - 2, this.currentPage + 3);
+    }
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page - 1;
     this.emitDataForShow();
   }
 }
